ca9f99ec0ed1b02af9a67aca89bd18ebcf27415a
Nisreen1994 <60920309+Nisreen1994@users.noreply.github.com>
2020-04-08 19:42:49 +0200

    back-end fun4

---
+++ src/main/java/com/example/fun4demo/Fun4demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fun4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fun4demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fun4demoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fun4demo/Fun4demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fun4demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fun4demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
